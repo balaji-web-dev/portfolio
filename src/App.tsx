ae0d3ec8c6c32407f8211731ab2ddcb6993cec97
balaji-web-dev <balls.ksj@gmail.com>
2023-05-10 18:02:05 +0530

    Chore(root): Resolve style lint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import logo from "./assets/images/logo.svg";
-import "./App.scss";
+import React from 'react';
+import logo from './assets/images/logo.svg';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Loading...</p>
-        <h1>Balaji's Personal Portfolio</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>Loading...</p>
+                <h1>Balaji's Personal Portfolio</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,59 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
-const mode = process.env.NODE_ENV || "development";
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  mode,
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif|eot|ttf|woff|woff2)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.svg$/,
-        use: "svg-url-loader",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+    mode,
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|eot|ttf|woff|woff2)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.svg$/,
+                use: 'svg-url-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    // alias: {
-    //   Assets: path.resolve(__dirname, "src/assets/"),
-    //   Images: path.resolve(__dirname, "src/assets/images/"),
-    // },
-  },
-  output: {
-    publicPath: "/",
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      hash: true,
-      filename: "../build/index.html",
-    }),
-  ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        // alias: {
+        //   Assets: path.resolve(__dirname, "src/assets/"),
+        //   Images: path.resolve(__dirname, "src/assets/images/"),
+        // },
+    },
+    output: {
+        publicPath: '/',
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            hash: true,
+            filename: '../build/index.html',
+        }),
+    ],
 };
