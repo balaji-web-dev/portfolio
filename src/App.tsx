93fb0c226b27ca95283a7401a74acfbed3cf225f
balaji-web-dev <balls.ksj@gmail.com>
2023-05-12 10:57:26 +0530

    Feat(client): Add fonts, default style scss and stylelint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import logo from './assets/images/logo.svg';
 import './App.scss';
+
+import React from 'react';
+
+import logo from './assets/images/logo.svg';
 
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
+        <div className='app'>
+            <header className='app-header'>
+                <img src={logo} className='app-logo' alt='logo' />
                 <p>Loading...</p>
                 <h1>Balaji's Personal Portfolio</h1>
             </header>
+            <section></section>
         </div>
     );
 }
